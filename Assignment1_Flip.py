7ea4093d1137309bca2246249ace4092acd416c9
unknown <2433985590@qq.com>
2023-10-01 12:17:16 +0200

    code update

--- Assignment1_Flip.py
+++ Assignment1_Flip.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[6]:
+# In[8]:
 
 
-import os
 from PIL import Image
 import numpy as np
 
@@ -17,11 +16,13 @@ im_array = np.array(im)
 #Modify the order of the array to get a new image flipping vertically
 im_up = im_array[::-1]
 im2 = Image.fromarray(im_up)
+im2.save("vertically.png")
 im2.show()
 
 #Modify the order of the array to get a new image flipping horizontally
 im_lr = im_array[:,::-1]
 im3 = Image.fromarray(im_lr)
+im3.save("horizontally.png")
 im3.show()
 
 
